0cae1684926fa7073a7a34a67ee7507add7040fc
Santhosh-kumar-p-123 <santhoshvijaypeter@gmail.com>
2026-01-01 12:55:55 +0530

    Updated Django settings

--- backend/settings.py
+++ backend/settings.py
@@ -145,7 +145,7 @@ import os
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
+        default=os.getenv("postgresql://postgres:anGFHHFIIatzrJDaxJRfuAsvvgxZMtXT@postgres.railway.internal:5432/railway"),
         conn_max_age=600,
         ssl_require=True
     )
